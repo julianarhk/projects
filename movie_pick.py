8574d9ea4543c0391623b367ace317d6634132bc
Juliana Hukuda <102271194+julianarhk@users.noreply.github.com>
2023-01-16 16:44:41 -0300

    Update movie_pick.py

--- movie_pick.py
+++ movie_pick.py
@@ -1,18 +1,18 @@
-mood = int(input("What's your mood today? Pick a number: \n 1- Giggly mood \n 2- Need inspiration \n"
+mood = int(input("What's your mood today? Pick a number: \n 1- Crazy alternative \n 2- Need inspiration \n"
                  " 3- I need to cry my eyes out \n 4- Just want to chill and not think much "
                  "\n 5- I want a thrill \n 6- Make me jump \n \n Answer: "))
 
 if mood == 1:
-    print("Watch \"Smiley Face\"")
+    print("Watch \"Crystal Fairy and the Magical Cactus\"")
 elif mood == 2:
     print("If \"The Intouchables\" doesn't inspire you, your soul might be dead")
 elif mood == 3:
-    print('\"Lovely Bones\" might do the job, if not you can always rewatch Titanic...')
+    print('\"Pursuit of Happyness\" might do the job, if not you can always rewatch Titanic...')
 elif mood == 4:
-    print('\"Amelie Poulain\" it is!')
+    print('\"Midnight in Paris\" it is!')
 elif mood == 5:
-    print('\"What keeps you alive\" is your movie tonight')
+    print('\"Wild Tales\" is your movie tonight')
 elif mood == 6:
-    print('\"Hush\" will make you bite your nails')
+    print('\"Smile\" will make you bite your nails')
 else:
     print('Invalid option you silly')
